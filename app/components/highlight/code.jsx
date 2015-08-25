98e9bd48f658630d11e1a2fb7aa9b6aa75b50add
Sun Xiqiu <sunxiqiu@admaster.com.cn>
2015-08-25 17:38:25 +0800

    change back highlight code

--- app/components/highlight/code.jsx
+++ app/components/highlight/code.jsx
@@ -2,22 +2,25 @@ require('highlight.js/styles/googlecode.css');
 
 var m = require("mithril");
 var classNames = require("classnames");
-var hljs = require('highlight.js');
+var hljs = require('highlight.js/lib/highlight');
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+
 var HighlightCode = {};
 
 HighlightCode.view = function(ctrl, props, children) {
+  var lang = props.lang || 'javascript';
 
   var styles = classNames({
     "hljs": true
   });
 
   var html = children.map(function(child) {
-    return hljs.highlight(props.lang, child).value;
+    return hljs.highlight(lang, child).value;
   });
 
   return (
     <pre class="highlight-code">
-      <code className={classNames(props.lang, styles)}>{m.trust(html)}</code>
+      <code className={classNames(lang, styles)}>{m.trust(html)}</code>
     </pre>
   );
 };
